d2788d5fe814e78e35cc1975833b8244c9f97a7a
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2024-12-23 18:01:10 +0100

    Add star effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Starfield from 'react-starfield';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[calc(100vh-80px)] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-9xl text-center font-[family-name:var(--font-geist-mono)]">MAGNUS GRINI</h1>
       </main>
