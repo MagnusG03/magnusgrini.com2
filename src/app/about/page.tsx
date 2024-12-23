00b1b6b63745381ba3ebf98bc2baffd2d73c7fa6
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2024-12-23 15:58:12 +0100

    Add about page info

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,25 @@
 export default function About() {
 
   return (
-    <div className="container min-h-screen mx-auto py-12">
-      <h1 className="mb-8 text-4xl font-bold">About</h1>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="container min-h-[calc(100vh-80px)] mx-auto py-12">
+      <h1 className="my-16 text-4xl font-bold text-center">About</h1>
+      <div className="flex flex-col gap-4">
+        <h3 className="text-2xl my-6">About me</h3>
+        <h3 className="text-lg">
+          I am in the final semester of my bachelor in Computer Science at The Norwegian University of Science and Technology (NTNU). So far in my studies, I have learned SQL, object-oriented programming, network programming, full-stack application development, machine learning and mobile development, and will also complete a bachelor's thesis throughout my final semester. I have also developed a handful of computer science related skills on my own time.
+        </h3>
+        <h3 className="text-lg">
+          I have an international background, with a total of 13 years in Canada, the United States, and the United Arab Emirates. Through my time in international schools, and from Living in these countries I have developed a strong international network, and have become more inclusive of other cultures and religions. Also, English and Norwegian have both become native to me, due to my international upbringing.
+        </h3>
+        <h3 className="text-2xl my-6">Education</h3>
+        <h3 className="text-lg">
+          2022 - 2025 Norwegian University of Science and Technology
+        </h3>
+        <h3 className="text-2xl my-6">Languages</h3>
+        <h3 className="text-lg">Norwegian - Native</h3>
+        <h3 className="text-lg">English - Native</h3>
+        <h3 className="text-lg">Spanish - A2</h3>
+        <h3 className="text-lg">German - A2</h3>
       </div>
     </div>
   )
