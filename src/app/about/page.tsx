f247a7b960feaae7435bf575e9dca88dcf724ab5
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2025-07-10 02:29:39 +0200

    Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,22 +6,17 @@ export default function About() {
       <div className="flex flex-col gap-4 sm:mx-0 mx-6 mb-4">
         <h3 className="text-2xl my-6">About me</h3>
         <h3 className="text-lg">
-          I have recently graduated from a bachelor’s degree in Computer Science at The
-          Norwegian University of Science and Technology (NTNU) and plan to continue my
-          education next semester with a master’s degree in Computer Science and Artificial
-          Intelligence. Throughout my studies, I have developed a broad variety of skills in
-          machine learning, algorithms and datastructures, SQL, object oriented programming in
-          multiple languages, and full stack mobile and web development in a variety of
-          frameworks. I have also developed a handful of computer science related skills on my
-          own time. Throughout the writing of my bachelor’s thesis, I also developed proficiency in
-          Unity, C#, Augmented Reality, AI based motion tracking, and game development.
+          I have recently graduated from a bachelor’s degree in Computer Science at The Norwegian University of Science and Technology (NTNU) and will begin a master’s degree in Artificial Intelligence this August. Throughout my studies, I have developed a broad variety of skills in machine learning, algorithms and datastructures, SQL, object oriented programming in multiple languages, and full stack mobile and web development in a variety of frameworks. I have also developed a handful of computer science related skills on my own time. Throughout the writing of my bachelor’s thesis, I also developed proficiency in Unity, C#, Augmented Reality, AI based motion tracking, and game development.
         </h3>
         <h3 className="text-lg">
           I have an international background, with a total of 13 years in Canada, the United States, and the United Arab Emirates. Through my time in international schools, and from Living in these countries I have developed a strong international network, and have become more inclusive of other cultures and religions. Also, English and Norwegian have both become native to me, due to my international upbringing.
         </h3>
         <h3 className="text-2xl my-6">Education</h3>
         <h3 className="text-lg">
-          2022 - 2025 Norwegian University of Science and Technology
+          2025 - 2027 Master’s Degree in Artificial Intelligence (NTNU)
+        </h3>
+        <h3 className="text-lg">
+          2022 - 2025 Bachelor’s Degree in Computer Science (NTNU)
         </h3>
         <h3 className="text-2xl my-6">Languages</h3>
         <h3 className="text-lg">Norwegian - Native</h3>
