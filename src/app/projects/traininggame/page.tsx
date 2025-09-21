b32b528889927c0bd6169f4af5328044b8bda13e
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2025-09-21 22:10:02 +0200

    Fix eslint problems

--- src/app/projects/traininggame/page.tsx
+++ src/app/projects/traininggame/page.tsx
@@ -10,7 +10,7 @@ export default function TrainingGame() {
       <h1 className="my-16 text-4xl font-bold text-center">Training Game</h1>
       <div className="mx-6 sm:mx-0">
         <h3 className="text-lg">
-          For my bachelor's thesis, in collaboration with the company Aible AS, my group partner and I created a motion tracking training game and calibration system with dynamic difficulty adjustment. The target demographic for this game was parkinson's patients, as they need physical excercise to mitigate some symptoms of their disease. Because of this target demographic, the dynamic difficulty adjustment is important, as the ability of the user can vary a lot. Unfortunately, I am under an NDA and can not talk about the underlying technologies behind the system, however, here are some screenshots from the game, to give an idea of what it looks like.
+          For my bachelor&apos;s thesis, in collaboration with the company Aible AS, my group partner and I created a motion tracking training game and calibration system with dynamic difficulty adjustment. The target demographic for this game was parkinson&apos;s patients, as they need physical exercise to mitigate some symptoms of their disease. Because of this target demographic, the dynamic difficulty adjustment is important, as the ability of the user can vary a lot. Unfortunately, I am under an NDA and cannot talk about the underlying technologies behind the system, however, here are some screenshots from the game, to give an idea of what it looks like.
         </h3>
         <div className="flex justify-center items-center mt-8">
           <Image
