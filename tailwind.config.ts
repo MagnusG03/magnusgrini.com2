c285aab24ac82a08d5ecfbaec772eb8a8100bd8f
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2025-10-01 20:45:55 +0200

    Add dinosaur game alpha

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,46 +56,7 @@ export default {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
-			keyframes: {
-				"triceratops-idle": {
-					"0%, 7.692%": { backgroundPosition: "0 0" },       // F1
-					"7.693%, 15.385%": { backgroundPosition: "-52px 0" },   // F2
-
-					"15.386%, 23.077%": { backgroundPosition: "0 0" },       // F1
-					"23.078%, 30.769%": { backgroundPosition: "-52px 0" },   // F2
-
-					"30.770%, 38.462%": { backgroundPosition: "0 0" },       // F1
-					"38.463%, 46.154%": { backgroundPosition: "-52px 0" },   // F2
-
-					"46.155%, 53.846%": { backgroundPosition: "0 0" },       // F1
-					"53.847%, 61.538%": { backgroundPosition: "-52px 0" },   // F2
-
-					"61.539%, 69.231%": { backgroundPosition: "0 0" },       // F1
-					"69.232%, 76.923%": { backgroundPosition: "-52px 0" },   // F2
-
-					"76.924%, 84.615%": { backgroundPosition: "0 0" },       // F1
-					"84.616%, 92.308%": { backgroundPosition: "-52px 0" },   // F2
-
-					"92.309%, 93.846%": { backgroundPosition: "-104px 0" },  // F3
-					"93.847%, 95.385%": { backgroundPosition: "-156px 0" },  // F4
-					"95.386%, 100%": { backgroundPosition: "-52px 0" },   // F2 (final hold)
-				},
-				"triceratops-run": {
-					"0%": { backgroundPosition: "0 0" },         // F1
-					"25%": { backgroundPosition: "-52px 0" },     // F2
-					"50%": { backgroundPosition: "-104px 0" },    // F3
-					"75%": { backgroundPosition: "-52px 0" },     // F2
-				}
-			},
-			animation: {
-				"triceratops-idle": "triceratops-idle 6500ms steps(1,end) infinite",
-				"triceratops-run": "triceratops-run 400ms steps(1,end) infinite",
-			},
 		},
 	},
-	safelist: [
-		'animate-triceratops-idle',
-		'animate-triceratops-run',
-	],
 	plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
 } satisfies Config;
